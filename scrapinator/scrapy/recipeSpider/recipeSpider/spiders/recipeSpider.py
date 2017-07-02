69e3ab1679669e22b34dbacb878c0d2590a8eac8
ryanp424 <ryanp424@gmail.com>
2017-07-02 22:39:57 +0000

    cleaning up file, and compressing pickled data

--- scrapinator/scrapy/recipeSpider/recipeSpider/spiders/recipeSpider.py
+++ scrapinator/scrapy/recipeSpider/recipeSpider/spiders/recipeSpider.py
@@ -1,18 +1,38 @@
+
+import scrapy
+import pickle
+import os
+import bz2
+
+from bs4 import BeautifulSoup
 from scrapy.selector import Selector
 from scrapy import Spider
-#from recipeSpider.items import Article
 
 
+class RecipeSpider(scrapy.Spider):
+    name = 'recipe'
+    start_urls = ['http://allrecipes.com/recipe/245119/biscuits-and-gravy-casserole/?internalSource=popular&referringContentType=home%20page&clickId=cardslot%2020', \
+    'http://allrecipes.com/recipe/18886/beccas-barbequed-beans/']
 
-
-class Recipespider(Spider):
-    name = "recipe"
-    allowed_domains = ["allrecipes.com"]
-    start_urls = ["http://allrecipes.com/recipe/245119/biscuits-and-gravy-casserole/?internalSource=popular&referringContentType=home%20page&clickId=cardslot%2020"]
-    def start_request(self):
-        urls = self.start_urls
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
     def parse(self, response):
-        print("yes")
+        bsObj = BeautifulSoup(response.body_as_unicode(), "html.parser")
+        title = bsObj.find("title").string
+
+        ingredList = bsObj.findAll("span", {"class": "recipe-ingred_txt", \
+        "itemprop": "ingredients" })
+
+        directions = bsObj.findAll("span", {"class": "recipe-directions__list--item"})
     
+        jsun = {'title': title, 'ingredList': ingredList, 'directions': directions }
+        
+        pickle_data = bz2.BZ2File('pickle_recipes/{0}.pickle.bz2'.format(title), 'w')        
+        pickle.dump(jsun, pickle_data)
+        pickle_data.close()
+    
+    
+        for item in response.css('a'):     
+            if item.re('similar_recipe_banner'):
+                href = item.css('a::attr(href)').extract()[0]
+                url = "http://allrecipes.com{0}".format(href)
+                yield response.follow(url, self.parse)
+
