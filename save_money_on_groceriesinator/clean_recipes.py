97a9b9f664dd4862f84f5cb1f74e7754d11f7da7
Ryan P <ryanp424@gmail.com>
2017-07-23 15:28:06 -0700

    moved pickle_recipes to sub-directory added directions to a list instead of just a dict to make it easier for Django templates

--- save_money_on_groceriesinator/clean_recipes.py
+++ save_money_on_groceriesinator/clean_recipes.py
@@ -9,13 +9,13 @@ import json
 from bs4 import BeautifulSoup
 
 # some initialization
-recipe_dir = os.getcwd() + "/pickle_recipes"
+recipe_dir = os.getcwd() + "/../pickle_recipes"
 output_file = open("fixtures/recipes.json", "w")
 json_output_array = []
 
 counter = 1
 for recipe in os.listdir(recipe_dir):
-    pickle_data = bz2.BZ2File("{0}/{1}".format(recipe_dir, recipe), 'r')
+    pickle_data = bz2.BZ2File("/{0}/{1}".format(recipe_dir, recipe), 'r')
     try:
         recipe_json = pickle.load(pickle_data)
     except EOFError:
@@ -28,15 +28,20 @@ for recipe in os.listdir(recipe_dir):
 
     # Getting rid of the <span></span> that was brought over from the web crawling.
     clean_ingredList = []
-    clean_directions = {}
+    directions_list = []
     for i in ingredList.find_all('span'):
         clean_ingredList.append(i.string)
     for count,i in enumerate(directions.find_all('span')):
-        key = str(count)
-        clean_directions[key] = i.string
+        count += 1
+        clean_directions = {}
+        if i.string != None:
+            key = str(count)
+            clean_directions['step'] = i.string
+            clean_directions['step_num'] = key
+            directions_list.append(clean_directions)
 
     # Create a new json model entry for the recipe to be imported by django
-    new_recipe = { 'model': 'core.recipes', 'pk': counter, 'fields': {'ingredList': clean_ingredList, 'directions': clean_directions, 'title': str(recipe_json['title'])}}
+    new_recipe = { 'model': 'core.recipes', 'pk': counter, 'fields': {'ingredList': clean_ingredList, 'directions': directions_list, 'title': str(recipe_json['title'])}}
     
     # add the recipe to json output array
     json_output_array.append(new_recipe)
@@ -46,7 +51,7 @@ for recipe in os.listdir(recipe_dir):
 
     # This section can be uncommented to limit number of files tested against, for testing
     """
-    if counter == 100:
+    if counter == 10:
         json.dump(json_output_array, output_file)        
 
 
