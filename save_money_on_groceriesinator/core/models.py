c132dce59b8b685b0d555aaa761b2b288b50be73
Ryan P <ryanp424@gmail.com>
2017-07-24 00:38:31 -0700

    updating class models

--- save_money_on_groceriesinator/core/models.py
+++ save_money_on_groceriesinator/core/models.py
@@ -1,25 +1,37 @@
 from django.db import models
 
 # Create your models here.
-class Recipes(models.Model):
-    # Recipes use multiple ingredients
-    ingredList = models.TextField()
-    #ingredList =
-    directions = models.TextField()
-    title      = models.TextField()
-    #original_url = models.URLField()
-
-
-class Pantry(models.Model):
-    # pantries have multiple ingredients
-    pass
-
 
 class Ingredient(models.Model):
     # the same ingredient will be used by many people, and recipes
-    pantry = models.ManyToManyField(Pantry)
-    recipe = models.ManyToManyField(Recipes)
-    amount = models.PositiveSmallIntegerField()
+    name = models.CharField(max_length=100)
 
-class Recipe_Ingredient(models.Model):
-    pass
+
+class Recipe(models.Model):
+    # Recipes use multiple ingredients
+    ingredList = models.ManyToManyField(Ingredient,
+                                        through='RecipeIngredient',
+                                        through_fields=('recipe', 'ingredient'))
+    directions = models.TextField()
+    title      = models.CharField(max_length=100)
+    #original_url = models.URLField()
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe)
+    ingredient = models.ForeignKey(Ingredient)
+    name = models.CharField(max_length=100)
+    amount = models.CharField(max_length=25)
+
+
+class Pantry(models.Model):
+    owner = models.CharField(max_length=50)
+    ingredList = models.ManyToManyField(Ingredient,
+                                        through='PantryIngredient',
+                                        through_fields=('pantry', 'ingredient'))
+
+class PantryIngredient(models.Model):
+    name = models.CharField(max_length=50)
+    pantry = models.ForeignKey(Pantry)
+    ingredient = models.ForeignKey(Ingredient)
+    amount = models.CharField(max_length=25)
